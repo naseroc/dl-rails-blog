b36c5327116aa3ebf0bafca3aed8d0a374bb1a1d
Carlo Casorzo <carloc@pagoranking.com>
2015-01-26 21:15:45 +0000

    Styled post views

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+  def excerpt(post)
+    truncate(post.content, length: 400, separator: ' ') do
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+ Post.create title:     Faker::Lorem.sentences(1).join(' '),
+             content:   Faker::Lorem.paragraphs(25, true).join(' ')
+end
